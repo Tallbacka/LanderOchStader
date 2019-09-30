b216b882d301616395a22ed34df5070a7a595db3
Jimmy Johansson <jim-joh@outlook.com>
2019-09-30 22:37:35 +0200

    Work on loading content JS

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@
 const cities = "json/stad.json";
 const countries = "json/land.json";
 
+document.getElementById('body').onload = function () {
+    getCountryData();
+    getCityData();
+}
 
 // Countries json data retrival
 async function getCountryData() {
@@ -26,45 +30,28 @@ async function getCityData() {
 
 // Appends the country buttons based on JSON data
 function drawCountry(countryData) {
-    var sidebar = document.getElementById('sideNav');
+    var countryContainer = document.getElementById('countryBar');
     
     for (let i = 0; i < Object.values(countryData).length; i++) {
         var button = document.createElement('button');
-        button.setAttribute('class', "dropdown-btn");
+        button.setAttribute('class', "w3-bar-item w3-button tablink");
         button.setAttribute('id', countryData[i].id);
-        button.setAttribute('onclick', "toggleButtons(this.id)")
-        button.innerHTML = (countryData[i].countryname);
-        
-        var icon = document.createElement('i')
-        icon.setAttribute('class', "fa fa-caret-down");
-        
-        var div = document.createElement('div');
-        div.setAttribute('id', "div" + countryData[i].id)
-        div.setAttribute('class', "dropdown-container")
-        
-        button.appendChild(icon);
-        sidebar.appendChild(button);
-        sidebar.insertAdjacentElement("beforeend", div);
+        button.setAttribute('onclick', "openCountry(event, this(id))");
+        button.innerHTML =countryData[i].countryname;
+        countryContainer.insertAdjacentElement("afterbegin", button)
     }
 }
 
 // Appends the city links based on JSON data
 function drawCities(cityData) {
-    var dropdown = document.getElementsByClassName('dropdown-btn');
-    console.log(dropdown.length);
-    console.log(cityData.length);
+    
 
     for (let i = 0; i < dropdown.length; i++) {
         for (let j = 0; j < Object.values(cityData).length; j++) {
             var a = document.createElement('a');
 
             if (dropdown[i].getAttribute("id") == cityData[j].countryid) {
-                var divCities = document.getElementById("div" + dropdown[i].id)
-
-                a.innerHTML = cityData[j].stadname;
-                a.setAttribute('href', "#");
-
-                divCities.appendChild(a);
+                
             } else {
                 console.log("nothing in common");
             }
@@ -72,16 +59,6 @@ function drawCities(cityData) {
     }
 }
 
-/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-function toggleButtons(buttonNumber) {
-    var div = document.getElementById("div" + buttonNumber)
-
-    if (div.style.display === "block") {
-        div.style.display = "none";
-    } else {
-        div.style.display = "block";
-    }
-}
 
 // Sorts the city data after population desc.
 function sortByPopulation(cityData) {
